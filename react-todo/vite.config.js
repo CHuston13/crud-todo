e45fa775773e3e7dc83d7248d49097f943f82c48
CHuston13 <CDHprogrammer@gmail.com>
2024-01-16 10:48:52 -0600

    reset

--- react-todo/vite.config.js
+++ react-todo/vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build:{
+    outDir:'dist',
+    base:'crud-fixed/crud-todo/react-todo',
   plugins: [react()],
-})
+}})
